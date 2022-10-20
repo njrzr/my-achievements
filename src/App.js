a775289237d948da02c86d20e7ddf8bd89a41654
Orlando Pernia <angelus078@gmail.com>
2022-10-20 18:10:39 -0500

    Change loader and some images deleted.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import User from './components/User';
 import Achievements from './components/Achievements';
 import Footer from './components/Footer';
 import { useState } from 'react';
-import Loader from './images/loader.webp'
 
 function App() {
   const [achievementFlag, setAchievementFlag] = useState(false);
@@ -10,8 +9,12 @@ function App() {
   return (
     <div className="App">
       { achievementFlag !== true &&
-        <div className="fixed flex justify-center items-center top-0 z-10 w-screen h-screen bg-secondary">
-          <img className="w-80 h-80 loader rounded-full" src={ Loader } alt="Loader"></img>
+        <div className="fixed flex justify-center items-center top-0 z-10 w-screen h-screen bg-secondary gap-4">
+          <div class="bars"></div>
+          <div class="bars"></div>
+          <div class="bars"></div>
+          <div class="bars"></div>
+          <div class="bars"></div>
         </div>
       }
       
